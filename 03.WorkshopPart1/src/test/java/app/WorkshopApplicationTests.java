7686dcc184e9d33c85a0176c407278e064fcc46e
PN <pnikolaykosev@gmail.com>
2020-03-12 16:10:06 +0100

    Changed project name

--- 03.WorkshopPart1/src/test/java/app/WorkshopApplicationTests.java
+++ 03.WorkshopPart1/src/test/java/app/WorkshopApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WorkshopApplicationPartOneApplicationTests {
+class WorkshopApplicationTests {
 
     @Test
     void contextLoads() {
