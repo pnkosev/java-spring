48e662f66bb00ba6219e5d539cfe5caa7e48c9c5
PN <pnikolaykosev@gmail.com>
2020-03-12 15:48:10 +0100

    Added structure, controllers and views

---
+++ 03.WorkshopPart1/src/main/java/app/web/controllers/AuthController.java
@@ -0,0 +1,20 @@
+package app.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class AuthController {
+
+    @GetMapping("/register")
+    public String register() {
+        return "auth/register";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "auth/login";
+    }
+}
