46eddc12dc6e62e347d4998248ff4642a9cff020
PN <pnikolaykosev@gmail.com>
2020-03-12 16:08:36 +0100

    Added header fragment

--- 03.WorkshopPart1/src/main/java/app/ServletInitializer.java
+++ 03.WorkshopPart1/src/main/java/app/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WorkshopPartOneApplication.class);
+        return application.sources(WorkshopApplication.class);
     }
 
 }
--- 03.WorkshopPart1/src/main/java/app/WorkshopApplication.java
+++ 03.WorkshopPart1/src/main/java/app/WorkshopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkshopPartOneApplication {
+public class WorkshopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WorkshopPartOneApplication.class, args);
+        SpringApplication.run(WorkshopApplication.class, args);
     }
 
 }
